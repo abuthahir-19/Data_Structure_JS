f931c6f4857ea06a58f6500b51ec7b34d32eff9f
SYED ABUTHAHIR A <62162295+Abu19CS@users.noreply.github.com>
2021-05-30 11:56:50 +0530

    Adding files with updated helper functions

--- Linked List/circularDouble.js
+++ Linked List/circularDouble.js
@@ -30,7 +30,19 @@ function main () {
         cDList.insert (val);
     }
     console.log ('Element of the Circular Doubly Linked List :');
+    cDList.printList ();
+    // console.log ('Deleting element at the 4 position :');
+    // cDList.removeAt (6);
+    // console.log ('After deletion :');
+    // cDList.printList();
+    console.log ('Inserting a new element called 65 at position 5 :');
+    cDList.insertAt (5, 65);
+    console.log ('After Inserting List elements are :')
     cDList.printList();
+    console.log ('Element at the 5th position :');
+    console.log (cDList.getElementAt (5));
+    console.log ('String version of output of the list elements :');
+    console.log (cDList.toString ());
 }
 
 class Node {
@@ -49,7 +61,7 @@ class CircularDoubleList {
 
     insert (element) {
         var node = new Node (element);
-        if (this.head === null && this.tail === null) {
+        if (!this.head) {
             this.head = node;
             this.tail = node;
         }
@@ -64,54 +76,95 @@ class CircularDoubleList {
 
 
     removeAt (position) {
-
+        let count = 1;
+        let curr = this.head;
+        while (count != position) {
+            curr = curr.next;
+            count++;
+        }
+        var nextNode = curr.next;
+        nextNode.prev = curr.prev;
+        curr.prev.next = nextNode;
     }
 
-    insert (position, element) {
-
+    insertAt (position, element) {
+        let count = 1;
+        var curr = this.head;
+        while (count != position) {
+            curr = curr.next;
+            count++;
+        }
+        var newNode = new Node (element);
+        var temp = curr.prev;
+        temp.next = newNode;
+        newNode.prev = temp;
+        newNode.next = curr;
+        curr.prev = newNode;
     }
 
     getElementAt (position) {
-
+        var count = 1;
+        var curr = this.head;
+        while (count != position) {
+            curr = curr.next;
+            count ++;
+        }
+        return curr.value;
     }
 
     toString () {
-
+        var str = '';
+        var curr = this.head;
+        str += curr.value + ' ';
+        curr = curr.next;
+        while (curr != this.head) {
+            str += curr.value + ' ';
+            curr = curr.next;
+        }
+        return str;
     }
 
-    indexOf (element) {
+    // indexOf (element) {
 
-    }
+    // }
 
-    delete (element) {
+    // delete (element) {
 
-    }
+    // }
 
-    deleteHead () {
+    // deleteHead () {
 
-    }
+    // }
 
-    isEmpty () {
+    // isEmpty () {
         
-    }
+    // }
 
-    getHeadElement () {
+    // getHeadElement () {
 
-    }
+    // }
 
-    getTailElement () {
+    // getTailElement () {
 
-    }
+    // }
 
     printList () {
         var List = [];
         var curr = this.head;
-        while (curr.next != this.head) {
-            List.push (curr.value);
+        List.push (curr.value);
+        curr = curr.next;
+        while (curr != this.head) {
+            List. push(curr.value);
             curr = curr.next;
         }
-        List.push (curr.value);
-        var print = console.log ;
-        print (...List);
+        console.log (...List);
     }
 }
+
+/**
+Input:
+50 70 30 60 90 20 80 65 55 25
+
+Output:
+
+**/
