1a152f88c06f29e04980e48e05c0973bb75fab9f
SYED ABUTHAHIR A <62162295+abuthahir-19@users.noreply.github.com>
2021-06-10 08:54:00 +0530

    Added new files and packages

---
+++ AVL Tree/searchAVL.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+var readable = fs.createReadStream (__dirname + '\\avlinput.txt');
+
+readable.resume();
+readable.setEncoding ('utf-8');
+
+var inputString = '';
+var currentLine = 0;
+
+readable.on ('data', data => {
+    inputString += data;
+});
+
+readable.on ('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main ();
+});
+
+function readLine () {
+    return inputString[currentLine++];
+}
+
+function main () {
+    var list = readLine().split(' ').map(Number);
+    console.log (...list);
+}
